b78ad9bee0fc96083f83becfa19331b0293da788
Nathan Marks <nathan@poweredbysearch.com>
2016-05-05 13:57:14 -0400

    Add Button test to try out class assertions

---
+++ src/components/Button.spec.js
@@ -0,0 +1,28 @@
+/* eslint-env mocha */
+import React from 'react';
+import { shallow } from 'enzyme';
+import { assert } from 'chai';
+import { createMuiTheme } from '../styles/theme';
+import { createStyleManager } from '../styles/styleManager';
+import Button, { styleSheet } from './Button';
+
+describe('<Button />', () => {
+  const theme = createMuiTheme();
+  const styleManager = createStyleManager({ theme });
+  const shallowWithContext = (node, context = {}) =>
+    shallow(node, { context: { theme, styleManager, ...context } });
+
+  it('should render a primary color raised button', () => {
+    const wrapper = shallowWithContext(
+      <Button type="raised" primary={true}>Hello World</Button>
+    );
+    assert.ok(
+      wrapper.hasClass(styleSheet.classes.raised),
+      'should have the raised class'
+    );
+    assert.ok(
+      wrapper.hasClass(styleSheet.classes.raisedPrimary),
+      'should have the raised primary class'
+    );
+  });
+});
--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ require('app-module-path').addPath(`${__dirname}'./../`);
 import Minimist from 'minimist';
 import Mocha from 'mocha';
 import Glob from 'glob';
-import { jsdom } from 'jsdom';
+// import { jsdom } from 'jsdom';
 
 /**
  * 1. Setup jsdom globals
@@ -11,21 +11,21 @@ import { jsdom } from 'jsdom';
  * 4. Run mocha
  */
 
-const exposedProperties = ['window', 'navigator', 'document'];
+// const exposedProperties = ['window', 'navigator', 'document'];
 
-global.document = jsdom('');
-global.window = document.defaultView;
+// global.document = jsdom('');
+// global.window = document.defaultView;
 
-Object.keys(document.defaultView).forEach((property) => {
-  if (typeof global[property] === 'undefined') {
-    exposedProperties.push(property);
-    global[property] = document.defaultView[property];
-  }
-});
+// Object.keys(document.defaultView).forEach((property) => {
+//   if (typeof global[property] === 'undefined') {
+//     exposedProperties.push(property);
+//     global[property] = document.defaultView[property];
+//   }
+// });
 
-global.navigator = {
-  userAgent: 'node.js',
-};
+// global.navigator = {
+//   userAgent: 'node.js',
+// };
 
 const argv = Minimist(process.argv.slice(2), {
   alias: {
